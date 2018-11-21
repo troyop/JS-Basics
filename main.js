6f454f3858931efbd2df707549576080c1ae56bd
classroom <classroom@feckc.org>
2018-11-21 11:17:31 -0600

    My First JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 function dateAndTime() {
     document.getElementById('dateAndTime').innerHTML = Date()    
 }
+
+function conosolePrint() {
+    console.log("hello Jon");
+}
+
+function alertPrint() {
+    alert("hello Jon");
+}
+    
+function documentPrint() {
+    document.write("hello jon")
+}
